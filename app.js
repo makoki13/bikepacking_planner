cb381975e532f744fd805cd883264e7eaa905b1a
makoki13 <pablo.makoki@gmail.com>
2022-06-23 13:09:15 +0200

    solo datos

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
